e69469f590fbc3ffc54e82d4054c6921cb07a6bc
Alexandra Tincu <tincu.alecsandra@gmail.com>
2020-01-23 17:35:40 +0200

    Added Hystrix

--- src/main/java/com/in28minutes/microservices/limitsservice/LimitsConfigurationController.java
+++ src/main/java/com/in28minutes/microservices/limitsservice/LimitsConfigurationController.java
@@ -2,11 +2,14 @@ package com.in28minutes.microservices.limitsservice;
 
 import com.in28minutes.microservices.limitsservice.bean.Configuration;
 import com.in28minutes.microservices.limitsservice.bean.LimitConfiguration;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@EnableHystrix
 public class LimitsConfigurationController {
 
     @Autowired
@@ -15,5 +18,15 @@ public class LimitsConfigurationController {
     @GetMapping("/limits")
     public LimitConfiguration retrieveLimitsFromConfiguration() {
         return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
+    }
+
+    @GetMapping("fault-tolerance-ex")
+    @HystrixCommand(fallbackMethod = "fallbackRetrieveConfiguration")
+    public LimitConfiguration retrieveConfiguration() {
+        throw new RuntimeException("NA");
+    }
+
+    public LimitConfiguration fallbackRetrieveConfiguration() {
+        return new LimitConfiguration(999, 9);
     }
 }
